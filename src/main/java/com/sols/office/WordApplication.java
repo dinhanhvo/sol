f3f61efb0496d7d6da8e58b4e1e1c15177576bd5
Dinh Anh Vo <dinhanhvo@gmail.com>
2023-11-01 16:34:08 +0700

    first commit

---
+++ src/main/java/com/sols/office/WordApplication.java
@@ -0,0 +1,14 @@
+package com.sols.office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class WordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sols/office/WordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sols.office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
